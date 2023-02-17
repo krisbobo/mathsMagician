750e80d27e05e8d3dc1956e59f3960ddbb5c9606
krisbobo <kiyongbohbo@gmail.com>
2023-02-17 21:36:37 +0100

    Feat: add functional parent App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
+import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
